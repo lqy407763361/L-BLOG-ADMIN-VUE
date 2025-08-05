27cd1580d36f3560a4f22902524b313aa896ebb9
lqy407763361 <407763361@qq.com>
2025-08-05 14:32:38 +0800

    0804

--- src/components/common/CommonSidebar.vue
+++ src/components/common/CommonSidebar.vue
@@ -1,14 +1,17 @@
 <script setup>
-// import { ref } from 'vue';
+import { ref } from 'vue';
 
-// 下拉导航列表
-// const slNameStatus = ref(true);
-const slNameHandleClick = (event) =>{
-      const slNameClass = event.target.closest('.sl-name');
-      if(slNameClass){
-            console.log('点击的项目:', slNameClass.textContent)
-      }
-}
+const menuStatus = ref({
+      article: false,
+      page: false,
+      message: false,
+      user: false,
+      admin: false,
+      system: false
+});
+const toggleSidebarMenu = (status) => {
+      menuStatus.value[status] = !menuStatus.value[status];
+};
 </script>
 
 <template>
@@ -19,59 +22,59 @@ const slNameHandleClick = (event) =>{
                         <li><small>管理员群组名称</small></li>
                   </ul>
             </div>
-            <ul class="sidebar-list" @click="slNameHandleClick">
+            <ul class="sidebar-list">
                   <li>
                         <router-link to="/" replace>
                               <Odometer /><span>仪表盘</span>
                         </router-link>
                   </li>
                   <li>
-                        <router-link to="/" class="sl-name" active-class="active-link">
+                        <router-link to="/" class="sl-name" @click="toggleSidebarMenu('article')">
                               <Document /><span>文章管理</span><ArrowRight />
                         </router-link>
-                        <ul class="sl-child">
+                        <ul class="sl-child" v-show="menuStatus.article">
                               <li><router-link to="/"><span>文章列表</span><DArrowRight /></router-link></li>
                               <li><router-link to="/"><span>文章分类列表</span><DArrowRight /></router-link></li>
                         </ul>
                   </li>
                   <li>
-                        <router-link to="/" class="sl-name" active-class="active-link">
+                        <router-link to="/" class="sl-name" @click="toggleSidebarMenu('page')">
                               <Reading /><span>单页管理</span><ArrowRight />
                         </router-link>
-                        <ul class="sl-child">
+                        <ul class="sl-child" v-show="menuStatus.page">
                               <li><router-link to="/"><span>关于我</span><DArrowRight /></router-link></li>
                         </ul>
                   </li>
                   <li>
-                        <router-link to="/" class="sl-name" active-class="active-link">
+                        <router-link to="/" class="sl-name" @click="toggleSidebarMenu('message')">
                               <Message /><span>信息管理</span><ArrowRight />
                         </router-link>
-                        <ul class="sl-child">
+                        <ul class="sl-child" v-show="menuStatus.message">
                               <li><router-link to="/"><span>我的新消息</span><DArrowRight /></router-link></li>
                         </ul>
                   </li>
                   <li>
-                        <router-link to="/" class="sl-name" active-class="active-link">
+                        <router-link to="/" class="sl-name" @click="toggleSidebarMenu('user')">
                               <User /><span>会员管理</span><ArrowRight />
                         </router-link>
-                        <ul class="sl-child">
+                        <ul class="sl-child" v-show="menuStatus.user">
                               <li><router-link to="/"><span>会员列表</span><DArrowRight /></router-link></li>
                         </ul>
                   </li>
                   <li>
-                        <router-link to="/" class="sl-name" active-class="active-link">
+                        <router-link to="/" class="sl-name" @click="toggleSidebarMenu('admin')">
                               <Postcard /><span>管理员管理</span><ArrowRight />
                         </router-link>
-                        <ul class="sl-child">
+                        <ul class="sl-child" v-show="menuStatus.admin">
                               <li><router-link to="/"><span>管理员列表</span><DArrowRight /></router-link></li>
                               <li><router-link to="/"><span>管理员群组列表</span><DArrowRight /></router-link></li>
                         </ul>
                   </li>
                   <li>
-                        <router-link to="/" class="sl-name" active-class="active-link">
+                        <router-link to="/" class="sl-name" @click="toggleSidebarMenu('system')">
                               <Setting /><span>系统管理</span><ArrowRight />
                         </router-link>
-                        <ul class="sl-child">
+                        <ul class="sl-child" v-show="menuStatus.system">
                               <li><router-link to="/"><span>网站设置</span><DArrowRight /></router-link></li>
                         </ul>
                   </li>
