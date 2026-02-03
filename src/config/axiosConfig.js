5328697114b863be91a5a3237011d603f9e13bc8
lqy407763361 <407763361@qq.com>
2026-02-03 17:26:12 +0800

    0203

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -21,16 +21,46 @@ httpRequest.interceptors.request.use(function(config){
 });
 
 //响应拦截器
-httpRequest.interceptors.response.use(function(response){
-    const data = response.data;
-    if(data){
-        if(data.code === '500'){
+httpRequest.interceptors.response.use(
+    function(response){
+        const data = response.data;
+        if(data && data.code === '500'){
             window.alert(data.msg);
             throw new Error(data.msg);
         }
-    }
 
-    return data;
-});
+        return data;
+    },
+
+    async function(error){
+        const originalRequest = error.config;
+
+        //如果身份验证过期，返回401状态码
+        if(error.response?.data?.code === '401' && !originalRequest._retry){
+            originalRequest._retry = true;
+
+            try{
+                //刷新accessToken
+                const res = await httpRequest.post('/adminRefreshAccessToken');
+                const newAccessToken = res.data.data;
+                localStorage.setItem('adminAccessToken', newAccessToken);
+
+                //重试请求
+                originalRequest.headers['adminAccessToken'] = newAccessToken;
+
+                return httpRequest(originalRequest);
+            }catch(refreshError){
+                //刷新失败，退出登录
+                localStorage.removeItem('adminAccessToken');
+                localStorage.removeItem('adminRefreshToken');
+                window.location.href = '/login';
+
+                return Promise.reject(refreshError);
+            }
+        }
+
+        return Promise.reject(error);
+    }
+);
 
 export default httpRequest
